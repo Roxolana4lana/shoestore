9577e8a5cd4c1ff4ddb09f6d6795a81133bbc650
Roxolana <roxolanapavlyk@gmail.com>
2019-03-22 10:02:13 +0100

    added styles

--- client/shoeapp/src/App.js
+++ client/shoeapp/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ShoeList from './components/ShoeList';
+import './styles/output.css'
+
 import AddNew from './components/AddNew';
+import ShoeList from './containers/ShoeList';
+import Main from './components/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <ShoeList/>
-     <AddNew/>
+<Main/>
       </div>
     );
   }
