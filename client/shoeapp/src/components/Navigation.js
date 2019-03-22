2aa53efa4db5fa131302758b8443b3aa7b90f345
Roxolana <roxolanapavlyk@gmail.com>
2019-03-22 16:22:22 +0100

    added navigation

---
+++ client/shoeapp/src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <div className='navigation'>
+      <ul>
+        <li><Link to=''></Link></li>
+        <li><Link to=''></Link></li>
+        <li><Link to=''></Link></li>
+      </ul>
+    </div>
+  )
+}
+export default withRouter(Navigation)
